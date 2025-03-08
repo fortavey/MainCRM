422afb5dc130d0ebbda4d5090182e600b569b1b4
fortavey <fortavey@gmail.com>
2025-03-08 07:29:50 +0300

    Initial Commit

---
+++ MainCRM/MainCRMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainCRMApp.swift
+//  MainCRM
+//
+//  Created by Main on 08.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MainCRMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
